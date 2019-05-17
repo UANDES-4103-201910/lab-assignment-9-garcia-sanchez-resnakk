bea68aad73fc194d9683c605975429b7120acd65
Tomas <tmgarcia@miuandes.cl>
2019-05-16 22:08:28 -0400

    vamoooooooo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,18 @@ class User < ApplicationRecord
 
 	validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
   	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
-
+  	devise :database_authenticatable, :registerable, :recoverable,
+  			:rememberable, :trackable, :validatable, :omniauthable,
+  			:omniauthable_provides => [:google_oauth2] 
+  	def self.from_omniauth(auth)
+  		where(provider:auth.provider , uid:auth.uid).first_or_create do |user|
+  			user.provider = auth.provider
+  			user.uid = auth.uid
+  			user.email = auth.info.email
+  			user.name = auth.info.name
+  			user.password = Devise.friendly_token[0,20]
+  		end
+  	end
 
 
 end
